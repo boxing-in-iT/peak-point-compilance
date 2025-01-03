9d699aa0b15e4d06723c39c05713b68fdf68bba7
cyber-boxer <kozakvolodymyr21@gmail.com>
2025-01-04 00:03:21 +0200

    add changes

--- src/store/api/chatApi.tsx
+++ src/store/api/chatApi.tsx
@@ -3,14 +3,23 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const chatApi = createApi({
   reducerPath: "chatApi",
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      // "https://lzcbdxce68.execute-api.eu-central-1.amazonaws.com/Prod/api/v1/chat/",
-      "https://18.227.114.8/",
+    baseUrl: "http://18.227.114.8/",
+    prepareHeaders: (headers) => {
+      const accessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnQUFBQUFCbmVFd2NPQkxwS0VpSGpZb3BNV1p3dW1EZ0ZEazBHQVEzb1lLTGIzdVJCT0M3MmFmeFZvLXhGN1JwellvS280b2ZkbEQ5S3VVTDd0UEF3bkRSQU9ENi1kR0VSZz09IiwiZXhwIjoxOTk1MTM3MDUyLjcwOTEyNX0.7TpwL8K9A8z_m3-oBd7XK1yyG3YDNwMVwGW3x6qn3Js";
+
+      // Добавляем токен в заголовки
+      if (accessToken) {
+        headers.set("Authorization", `Bearer ${accessToken}`);
+      }
+
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     sendMessage: builder.mutation<any, { user_query: string }>({
       query: (message) => ({
-        url: "message",
+        url: "api/v1/chat/message",
         method: "POST",
         body: message,
       }),
