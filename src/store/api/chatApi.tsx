7181fd8e07d6578ccb1a48864d11ec9f9cfc8abc
cyber-boxer <kozakvolodymyr21@gmail.com>
2025-01-08 22:43:24 +0200

    change base url

--- src/store/api/chatApi.tsx
+++ src/store/api/chatApi.tsx
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const chatApi = createApi({
   reducerPath: "chatApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://18.227.114.8/",
+    // baseUrl: "http://18.227.114.8/",
+    baseUrl: "https://ebn7ubhhzb.execute-api.us-east-1.amazonaws.com/Prod/",
     prepareHeaders: (headers) => {
       const accessToken =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnQUFBQUFCbmVFd2NPQkxwS0VpSGpZb3BNV1p3dW1EZ0ZEazBHQVEzb1lLTGIzdVJCT0M3MmFmeFZvLXhGN1JwellvS280b2ZkbEQ5S3VVTDd0UEF3bkRSQU9ENi1kR0VSZz09IiwiZXhwIjoxOTk1MTM3MDUyLjcwOTEyNX0.7TpwL8K9A8z_m3-oBd7XK1yyG3YDNwMVwGW3x6qn3Js";
